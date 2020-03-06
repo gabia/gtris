bfe49e6a01b57654270c4568dc1f8e2d4bf617c7
jnh <jnh@gabia.com>
2020-03-06 14:29:05 +0900

    global css 적용

--- vue.config.js
+++ vue.config.js
@@ -12,7 +12,12 @@ module.exports = {
   css: {
     loaderOptions: {
       scss: {
-        prependData: `@import url('https://static.gabia.com/fonts/icons/v1.0.1/style.css');`
+        prependData: `
+          @import "@/assets/sass/base/_variables.scss";
+          @import "@/assets/sass/mixins/_clearfix.scss";
+          @import "@/assets/sass/mixins/_truncate.scss";
+          @import url('https://static.gabia.com/fonts/icons/v1.0.1/style.css');
+          `
       }
     },
     sourceMap: true
