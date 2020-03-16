64b42b7a4f0883751ebfffc863c63f0a1110d75e
조영무 <jym@gabia.com>
2020-03-16 09:09:01 +0900

    버튼 수정

--- src/components/button.vue
+++ src/components/button.vue
@@ -1,40 +1,179 @@
 <template>
     <div>
-        <button type="button" class="gt-btn gt-btn-primary">Priy({{output}})</button>
+        <button 
+          v-if="type=='icon'"
+          class="gt-btn gt-btn-icon"
+          @click="eventClick"
+        >
+          <i 
+            class="gis"
+            :class="[icon? 'gis gi-'+icon: '']"
+          ></i>
+        </button>
+        <button 
+          v-if="type!='icon'"
+          class="gt-btn"
+          :type="type"
+          :disabled="disabled"
+          :class="[indicator? 'gt-btn-'+indicator: '']"
+          @click="eventClick"
+        >
+          {{text}}
+        </button>
+        
     </div>
 </template>
 
 <script>
-import common from '@/assets/js/common.js'
+//import common from '@/assets/js/common.js'
 
 export default {
   name: 'gtbutton',
   data() {
     return {
-        output: ""
+      isOnComposition: false
     }
   },
-  props: {
-   awesomeUrl : {
-      type: String,
-      default: 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css'
-    },    
+  props: {    
+    type: { type: String, required: false, default: null},
+    text: { type: String, required: true },
+    disabled: { type: Boolean, required: false, default: false },
+    indicator: { type: String, required: false, default: null },
+    icon: { type: String, required: false, default: null }/* ,
+    pill: {type: String, required: false, default: null },
+    float: {type: String, required: false, default: null },
+    size: { type: String, required: false, default: null } */
   },
-  mounted: function () {
-    const awesomeExist = !!document.getElementById('font-awesome')
-    if (!awesomeExist) {
-      const link = document.createElement('link')
-      link.src = this.awesomeUrl
-      link.id = 'font-awesome'
-      document.body.appendChild(link)
+  methods: {
+    eventClick(evt) {
+      this.$emit('click', evt);
     }
-    this.output = common.test();
   }
 }
 </script>
 
 <style lang="scss">
-.test {
-    color:aqua
+.gt-btn {
+  display: inline-flex;
+  flex-shrink: 0;
+  align-items: center;
+  position: relative;
+  color: #999;
+  background: #fff;
+  height: 40px;
+  min-width: 50px;
+  padding-left: 16px;
+  padding-right: 16px;
+  font-size: 16px;
+  border-radius: 0;
+  font-weight: 400;
+  border: 1px solid transparent;
+  transition: color 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms, background-color 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms, border-color 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms;
+  box-sizing: border-box;
+  justify-content: center;
+  vertical-align: middle;
+  overflow: hidden;
+  white-space: nowrap;
+  cursor: pointer;
+  user-select: none;
+  outline: none;
+
+  /* hover */
+  &:hover,
+  &:focus {
+    color: #666;
+    background: #f8f8f8;
+  }
+
+  /* disabled */
+  &:disabled {
+    cursor: default;
+    opacity: .3;
+    background: none;
+  }
+  &:hover[disabled] {
+    color: #999;
+  }
+
+  /* outline button */
+  &.gt-btn-outline {
+    border-color: #ddd;
+
+    &:hover, &:focus {
+      border-color: #aaa;
+      background-color: #fff;
+    }
+
+    &:hover[disabled], &:focus[disabled] {
+      border-color: #ddd;
+    }
+  }
+
+  /* primary */
+  &.gt-btn-primary {
+    background-color: #2985db;
+    border-color: #2985db;
+    color: #fff;
+
+    &:hover, &:focus {
+      background-color: #056ac9;
+      border-color: #056ac9;
+      color: #fff;
+    }
+
+    &:hover[disabled], &:focus[disabled] {
+      background-color: #2985db;
+      border-color: #2985db;
+      color: #fff;
+    }
+  }
+
+  /* secondary */
+  &.gt-btn-secondary {
+    background-color: #6c757d;
+    border-color: #6c757d;
+    color: #fff;
+
+    &:hover, &:focus {
+      background-color: #5a6268;
+      border-color: #5a6268;
+      color: #fff;
+    }
+
+    &:hover[disabled], &:focus[disabled] {
+      background-color: #5a6268;
+      border-color: #5a6268;
+      color: #fff;
+    }
+  }
+
+  /* danger */
+  &.gt-btn-danger {
+    background-color: #dc3545;
+    border-color: #dc3545;
+    color: #fff;
+
+    &:hover, &:focus {
+      background-color: #c82333;
+      border-color: #c82333;
+      color: #fff;
+    }
+
+    &:hover[disabled], &:focus[disabled] {
+      background-color: #dc3545;
+      border-color: #dc3545;
+      color: #fff;
+    }
+  }
+
+  /* icon button */
+  &.gt-btn-icon {
+    width: 40px;
+    min-width: 40px;
+    height: 40px;
+    border-radius: 50%;
+  }
 }
+
+
 </style>
