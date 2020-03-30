62633ad3773e982a96606f3bf69736b2b5bf6040
leejeongkyu <ljk@gabia.com>
2020-03-30 17:17:20 +0900

    collapse 경로 변경 , input 추가

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import gtbutton from './components/button.vue'
-import gtcollaps from './components/Collapse.vue'
+import gtcollaps from './components/Collapse/Collapse.vue'
 import gttooltip from "./components/tooltip.vue"
 import gtinput from "./components/Input.vue"
 import '@/assets/sass/gtris.scss'
