9496450fa5209489a644660f07d9e785fbf59adf
leejeongkyu <ljk@gabia.com>
2020-03-30 17:07:00 +0900

    gtris input 추가

---
+++ src/components/Input.vue
@@ -0,0 +1,77 @@
+<template>
+  <input
+    ref="input"
+    class = "hw-input"
+    :value="defaultValue"
+    :placeholder = "placeholder"
+    :class = "[customClass, size]"
+    :style = "{width: width, height: height}"
+    :autofocus = autofocus
+    @keypress.enter="$_pressEnter"
+    @keyup="$_keyup"
+  >
+</template>
+
+<script>
+export default {
+  name: "gt-input",  
+  watch: {
+    defaultValue(newVal) {
+      this.$refs.input.value = newVal;
+    }
+  },
+  props: {
+    defaultValue: {type: String, required: false, default: ''},
+    placeholder: {type: String, required: false, default: ''},
+    size: {type: String, required: false, default: ''},
+    customClass: { type: String, required: false, default: '' },
+    width: { type: String, required: false, default: '' },
+    height: { type: String, required: false, default: '' },
+    autofocus: { type: Boolean, required: false, default: false },
+  },
+  methods: {
+    $_pressEnter(e) {
+      this.$emit('pressEnter', e.target.value);
+    },
+    $_keyup(e) {
+      this.$emit('keyup', e.target.value);
+      this.$emit('input', e.target.value);
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.hw-input {
+  display: inline-block;
+  width: 100%;
+  min-height: 34px;
+  font-size: 14px;
+  padding: 0 10px;
+  border: 1px solid #d6d6d6;
+  transition-duration: .2s;
+  box-sizing: border-box;
+  &:hover {
+    border-color: #c0c4cc;
+  }
+  &:focus {
+    border-color: $primary;
+    outline: none;
+  }
+  &::placeholder {
+    color: #acacac;
+  }
+  &[disabled] {
+    background: #f8f8f8;
+    border-color: #d6d6d6;
+    user-select: none;
+  }
+  &.sm {
+    height: 30px;
+    min-height: 30px;
+  }
+  &.lg {
+    height: 40px;
+  }
+}
+</style>
--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import gtbutton from './components/button.vue'
 import gtcollaps from './components/Collapse.vue'
 import gttooltip from "./components/tooltip.vue"
+import gtinput from "./components/Input.vue"
 import '@/assets/sass/gtris.scss'
 
 const install = (Vue) => {
   Vue.component(gtbutton.name, gtbutton),
   Vue.component(gtcollaps.name, gtcollaps)
   Vue.component(gttooltip.name, gttooltip)  
+  Vue.component(gtinput.name, gtinput)    
 }
 
 // auto install if used in browser
