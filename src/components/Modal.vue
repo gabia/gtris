281dfb7833ada42a2b06140a95c84e1c5efdd48e
jnh <jnh@gabia.com>
2020-05-12 13:59:22 +0900

    modal 작업 중 파일

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,16 +1,20 @@
 <template>
-  <div class="gt-modal" :class="customClass" v-show="isShowing">
-    <div class="gt-modal-head" v-if="hasTitle">
-      <slot name="head"/>
-    </div>
-    <div class="gt-modal-body">
-      <slot name="body"/>
-    </div>
-    <div class="gt-modal-foot">
-      <slot name="foot">
-        <gtbutton @click="$_close" indicator="secondary">취소</gtbutton>
-        <gtbutton @click="$_close" indicator="primary">확인</gtbutton>
-      </slot>
+  <div v-show="isShowing" ref="modalRoot">
+    <div class="gt-modal-wrapper">
+      <div class="gt-modal" :class="customClass" :style="{flex: `0 0 ${width}`}">
+        <div class="gt-modal-head" v-if="hasTitle">
+          <slot name="head"/>
+        </div>
+        <div class="gt-modal-body">
+          <slot name="body"/>
+        </div>
+        <div class="gt-modal-foot">
+          <slot name="foot">
+            <gtbutton @click="$_close" indicator="secondary">취소</gtbutton>
+            <gtbutton @click="$_close" indicator="primary">확인</gtbutton>
+          </slot>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -24,27 +28,44 @@ export default {
   },
   data() {
     return {
-      isShowing: true,
-      id: this.name || `gt-modal-${Math.random().toString(36).substr(2, 8)}`
+      isShowing: false
     }
   },
   props: {
-    name: {type: [String, Number], required: false, default: null},
-    customClass: {type: String, required: false, default: null}
+    name: {type: [String, Number], required: true},
+    customClass: {type: String, required: false, default: null},
+    width: {type: String, required: false, default: '700px'}
   },
   computed: {
     hasTitle() {
       return this.$slots.head;
     },
   },
+  created() {
+    this.$eventHub.$on(`gt::opened::modal-${this.name}`, this.$_open);
+    this.$eventHub.$on(`gt::closed::modal-${this.name}`, this.$_close);
+  },
+  destroyed() {
+    this.$eventHub.$off(`gt::opened::modal-${this.name}`, this.$_open);
+    this.$eventHub.$off(`gt::closed::modal-${this.name}`, this.$_close);
+  },
   methods: {
     $_open() {
       this.isShowing = true;
-      this.$parent.$emit('gt::opened::modal', this.id);
+      this.$_createBackdrop();
     },
     $_close() {
-      this.isShowing = false;
-      this.$parent.$emit('gt::closed::modal', this.id);
+      let backdrop = document.getElementById(`gt_modal_backdroup_${this.name}`);
+      backdrop.parentNode.removeChild(backdrop);
+      Object.assign(this.$data, this.$options.data());
+    },
+    $_createBackdrop() {
+      let backdrop = document.createElement("div");
+      backdrop.setAttribute("id", `gt_modal_backdroup_${this.name}`);
+      backdrop.setAttribute("class", "gt-modal-backdrop");
+      backdrop.setAttribute("style", "position: fixed; left: 0; top: 0; width: 100%; height: 100%; opacity: 0.5; background: #000; z-index: 5000;");
+      backdrop.addEventListener('click', this.$_close, false);
+      this.$refs.modalRoot.appendChild(backdrop);
     }
   },
   watch: {
@@ -53,18 +74,36 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.gt-modal-head,
-.gt-modal-body {
+.gt-modal-wrapper {
+  position: fixed;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  overflow: auto;
+  margin: 0;
+  z-index: 5001;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.gt-modal {
   padding: 1em;
+  margin: 10vh 0;
+  border: 1px solid $modal-border;
+  background: #fff;
+  color: $modal-font;
 }
 .gt-modal-head {
+  padding-bottom: 1em;
   border-bottom: 1px solid $modal-border;
 }
 .gt-modal-body {
-
+  padding: 1em 0;
 }
 .gt-modal-foot {
   text-align: center;
+  padding-bottom: 1em;
   .gt-btn + .gt-btn {
     margin-left: 10px;
   }
