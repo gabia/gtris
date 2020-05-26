93174052c9dbe637022d8ba161b6958162dd366c
jnh <jnh@gabia.com>
2020-05-26 15:22:32 +0900

    define colors

--- vue.config.js
+++ vue.config.js
@@ -13,7 +13,7 @@ module.exports = {
     loaderOptions: {
       scss: {
         prependData: `
-          @import "@/assets/sass/base/_defineColors.scss";
+          @import "@/assets/sass/base/_colors.scss";
           @import "@/assets/sass/mixins/_clearfix.scss";
           @import "@/assets/sass/mixins/_truncate.scss";
           @import url(https://static.gabia.com/fonts/icons/v1.0.1/style.css);
