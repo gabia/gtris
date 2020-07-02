5e320f94723030187b9cc6ed768419d6b13b737c
leejeongkyu <ljk@gabia.com>
2020-07-02 11:23:47 +0900

    index.js Toast 경로 문제 버그 픽스

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import gtcollapse from './components/Collapse/Collapse.vue'
 import gtcollapseitem from './components/Collapse/CollapseItem.vue'
 import gttooltip from "./components/Tooltip/tooltip.vue"
 import gtinput from "./components/Input/Input.vue"
-import gttoast from "./components/Toast/toast.vue"
+import gttoast from "./components/Toast/Toast.vue"
 import gtpagination from "./components/Pagination/Pagination.vue"
 import gtpaginationscroll from "./components/Pagination/PaginationInfinite.js"
 import gtModal from "./components/Modal/Modal.vue"
